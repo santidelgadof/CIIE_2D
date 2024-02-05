55b19f3a0d412961f8dc01495ba8033db5b555b0
Cristian Gonzalez Trillo <cristian.gonzalez@nomasystems.com>
2024-02-05 17:47:16 +0100

    feat: implementada pantalla inicial. Closes #1

---
+++ tutorial.py
@@ -0,0 +1,41 @@
+# ---------------------------
+# Importacion de los módulos
+# ---------------------------
+
+import pygame
+from pygame.locals import *
+import sys
+
+# -----------
+# Constantes
+# -----------
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+# ------------------------------
+# Clases y Funciones utilizadas
+# ------------------------------
+
+
+# ------------------------------
+# Funcion principal del juego
+# ------------------------------
+
+
+def main():
+    pygame.init()
+    # creamos la ventana y le indicamos un titulo:
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("tutorial pygame parte 2")
+
+    # el bucle principal del juego
+    while True:
+        # Posibles entradas del teclado y mouse
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+
+if __name__ == "__main__":
+    main()
