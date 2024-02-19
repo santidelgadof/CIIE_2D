4737d841b49cb8e414c394d351134e194fa8eec5
Clara Cidrás Fernández <91012917+claracidras@users.noreply.github.com>
2024-02-19 16:23:59 +0100

    Added music, sounds, background and new funtionalities

--- Tetris/Tetris.py
+++ Tetris/Tetris.py
@@ -2,56 +2,58 @@ import pygame
 import random
 import time
 
-# Inicializar Pygame
+
 pygame.init()
 
-# Definir las variables globales
-WINDOW_WIDTH = 800  # Ancho de la ventana general
-WINDOW_HEIGHT = 800  # Alto de la ventana general
-BOARD_WIDTH = 200  # Ancho del tablero del juego
-BOARD_HEIGHT = 500  # Alto del tablero del juego
-BOARD_OFFSET_X = (WINDOW_WIDTH - BOARD_WIDTH) // 2  # Desplazamiento X para centrar el tablero
+# Dimensións ventá 
+WINDOW_WIDTH = 800  
+WINDOW_HEIGHT = 800  
+
+# Dimensións tableiro 
+BOARD_WIDTH = 200 
+BOARD_HEIGHT = 500 
+
+BOARD_OFFSET_X = (WINDOW_WIDTH - BOARD_WIDTH) // 2 
 BLOCK_SIZE = 20
+
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
 BLACK = (0, 0, 0)
 RED_LIGHT = (255, 128, 128)
 FONT_SIZE = 24
-SCORE_FONT_SIZE = 18
-TIMER_FONT_SIZE = 24
 
-# Definir la forma de los bloques de Tetris y sus colores
-tetrominoes = [
+# Formas das pezas
+blocks = [
     [[[0, 2, 2],
-      [2, 2, 0]],  # Tetromino "S"
-     (0, 0, 255)],  # Color Azul
+      [2, 2, 0]],  # Peza "S"
+     (0, 0, 255)],  # Azul
 
     [[[3, 3, 0],
-      [0, 3, 3]],  # Tetromino "Z"
-     (255, 165, 0)],  # Color Naranja
+      [0, 3, 3]],  # Peza "Z"
+     (255, 165, 0)],  #  Naranxa
 
     [[[4, 4, 4],
-      [0, 4, 0]],  # Tetromino "T"
-     (128, 0, 128)],  # Color Morado
+      [0, 4, 0]],  # Peza "T"
+     (150, 75, 0)],  # Marrón
 
     [[[0, 0, 5],
-      [5, 5, 5]],  # Tetromino "L"
-     (0, 255, 0)],  # Color Verde
+      [5, 5, 5]],  # Peza "L"
+     (0, 255, 0)],  # Verde
 
     [[[6, 6],
-      [6, 6]],  # Tetromino "O"
-     (255, 255, 0)],  # Color Amarillo
+      [6, 6]],  # Peza "O"
+     (255, 255, 0)],  # Amarelo
 
-    [[[7, 7, 7, 7]],  # Tetromino "I"
-     (255, 0, 0)]  # Color Rojo
+    [[[7, 7, 7, 7]],  # Peza "I"
+     (255, 0, 0)]  # Vermalle
 ]
 
-# Clase para representar el bloque de Tetris
-class Tetromino:
+
+class Block:
     def __init__(self):
-        self.x = random.randint(2, BOARD_WIDTH // BLOCK_SIZE - 4) * BLOCK_SIZE + BOARD_OFFSET_X  # Limitar la posición inicial
+        self.x = random.randint(2, BOARD_WIDTH // BLOCK_SIZE - 4) * BLOCK_SIZE + BOARD_OFFSET_X 
         self.y = 0
-        self.shape, self.color = random.choice(tetrominoes)
+        self.shape, self.color = random.choice(blocks)
         self.falling = True
     
     def draw(self, surface):
@@ -109,138 +111,192 @@ class Tetromino:
             self.shape = [[current_shape[j][i] for j in range(len(current_shape))] for i in range(len(current_shape[0]))]
             self.shape.reverse()
 
-# Función principal del juego
+
+
 def main():
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Tetris")
     clock = pygame.time.Clock()
     font = pygame.font.Font(None, FONT_SIZE)
-    score_font = pygame.font.Font(None, SCORE_FONT_SIZE)
-    timer_font = pygame.font.Font(None, TIMER_FONT_SIZE)
-
-    game_over = False
-
-    # Inicializar el tablero
-    board = [[None] * (BOARD_WIDTH // BLOCK_SIZE) for _ in range(BOARD_HEIGHT // BLOCK_SIZE)]
-
-    # Lista de tetrominos activos
-    tetrominos = [Tetromino()]
+    score_font = pygame.font.Font(None, FONT_SIZE)
+    timer_font = pygame.font.Font(None, FONT_SIZE)
 
     start_time = time.time()
-    increase_speed_interval = 5  # Aumentar la velocidad cada 5 segundos
-    speed_increase_factor = 0  # Factor de aumento de velocidad
+
+    game_over = False
+    game_over_sound_played = False  
+    ended = False
+
+    # Inicializar tableiro
+    board = [[None] * (BOARD_WIDTH // BLOCK_SIZE) for _ in range(BOARD_HEIGHT // BLOCK_SIZE)]
+
+    # Lista de bloques activos
+    blocks = [Block()]
+
+    # Velocidade
+    speed = 5  # Velocidad inicial de caída das pezas
+    increase_interval = 5  # Intervalo de tempo no que aumenta a velocidade
+    last_increase_time = time.time()  
 
     # Contadores
     score = 0
-    countdown_timer = 30
+    countdown_timer = 60
+
+    # Imaxe de fondo
+    background_image = pygame.image.load('background.jpeg')
+
+    # Música
+    pygame.mixer.music.load('tetris_song.ogg')
+    pygame.mixer.music.play(-1)  
+
+    # Efectos de sonido
+    clear_row_sound = pygame.mixer.Sound('line.wav')
+    piece_drop_sound = pygame.mixer.Sound('drop.mp3')
+    win_sound = pygame.mixer.Sound('win.mp3')
+    lose_sound = pygame.mixer.Sound('lose.mp3')
 
     running = True
     while running:
-        screen.fill(GRAY)  # Color de fondo de la ventana
+        screen.blit(background_image, (0, 0))  
 
-        # Dibujar área del tablero
+        # Área tableiro
         pygame.draw.rect(screen, WHITE, (BOARD_OFFSET_X, 0, BOARD_WIDTH, BOARD_HEIGHT))
 
-        # Dibujar línea que delimita el tablero del juego
+        # Límite tableiro
         pygame.draw.line(screen, BLACK, (BOARD_OFFSET_X, 0), (BOARD_OFFSET_X, BOARD_HEIGHT), 2)
         pygame.draw.line(screen, BLACK, (BOARD_OFFSET_X + BOARD_WIDTH, 0), (BOARD_OFFSET_X + BOARD_WIDTH, BOARD_HEIGHT), 2)
 
-        # Obtener el tiempo actual en segundos
+        current_time = time.time()
         elapsed_time = time.time() - start_time
-        if elapsed_time >= 1:  # Restar una unidad cada segundo
-            countdown_timer = max(countdown_timer - 1, 0)
-            start_time = time.time()  # Reiniciar el tiempo de inicio
 
-        # Dibujar el contador de tiempo
-        timer_text = timer_font.render(f"Tiempo: {countdown_timer}", True, BLACK)
-        screen.blit(timer_text, (10, 10))
+        if current_time >= increase_interval + last_increase_time:
+            speed *= 1.125  # Aumentar velocidade
+            last_increase_time = current_time  
 
-        # Dibujar el contador de puntuación
+        clock.tick(speed)  
+
+        if elapsed_time >= 1:  
+            if not game_over or countdown_timer > 0:  
+                countdown_timer = max(countdown_timer - 1, 0)
+                start_time = time.time()  
+
+        # Mostrar tempo restante
+        if not game_over or countdown_timer > 0: 
+            timer_text = timer_font.render(f"Tiempo: {countdown_timer}", True, BLACK)
+            screen.blit(timer_text, (10, 10))
+
+        # Mostrar puntuación
         score_text = score_font.render(f"Puntuación: {score}", True, BLACK)
-        screen.blit(score_text, (WINDOW_WIDTH - score_text.get_width() - 10, 10))
+        screen.blit(score_text, (10, 30))
 
-        # Manejo de eventos
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if not game_over:
                     if event.key == pygame.K_LEFT:
-                        # Mover el tetromino hacia la izquierda
-                        tetrominos[-1].move_left(board)
+                        # Mover peza á esquerda
+                        blocks[-1].move_left(board)
                     elif event.key == pygame.K_RIGHT:
-                        # Mover el tetromino hacia la derecha
-                        tetrominos[-1].move_right(board)
+                        # Mover peza á dereita
+                        blocks[-1].move_right(board)
                     elif event.key == pygame.K_SPACE:
-                        # Rotar el tetromino
-                        tetrominos[-1].rotate(board)
+                        # Rotar peza 
+                        blocks[-1].rotate(board)
                     elif event.key == pygame.K_DOWN:
-                        # Mover la pieza hacia abajo rápidamente
-                        while not tetrominos[-1].collide(board):
-                            tetrominos[-1].move_down()
+                        # Baixar peza
+                        while not blocks[-1].collide(board):
+                            blocks[-1].move_down()
 
         if not game_over:
-            # Obtener el tetromino actual
-            current_tetromino = tetrominos[-1]
+            
+            current_block = blocks[-1]
 
-            # Mover el tetromino hacia abajo
-            if current_tetromino.falling:
-                current_tetromino.move_down()
+            if current_block.falling:
+                current_block.move_down()
 
-            # Verificar colisión
-            if current_tetromino.collide(board):
-                # Agregar el tetromino actual al tablero
-                for i in range(len(current_tetromino.shape)):
-                    for j in range(len(current_tetromino.shape[i])):
-                        if current_tetromino.shape[i][j]:
-                            board[current_tetromino.y // BLOCK_SIZE + i][(current_tetromino.x - BOARD_OFFSET_X) // BLOCK_SIZE + j] = current_tetromino.color
+            # Comprobar colisión
+            if current_block.collide(board):
+                # Engadimos a peza actual ao tableiro
+                for i in range(len(current_block.shape)):
+                    for j in range(len(current_block.shape[i])):
+                        if current_block.shape[i][j]:
+                            board[current_block.y // BLOCK_SIZE + i][(current_block.x - BOARD_OFFSET_X) // BLOCK_SIZE + j] = current_block.color
 
-                # Eliminar filas completas y reorganizar las filas restantes
+                # Eliminar as filas que estén completas
                 rows_to_delete = set()
                 for i in range(len(board)):
                     if all(board[i]):
                         rows_to_delete.add(i)
-                        score += 100  # Sumar puntos por cada fila eliminada
+                        score += 100  
 
                 for row in rows_to_delete:
                     del board[row]
                     board.insert(0, [None] * (BOARD_WIDTH // BLOCK_SIZE))
+                    clear_row_sound.play() 
 
-                # Crear un nuevo tetromino si no hay filas completas
+                # Lanzar unha nova peza cando non queden máis liñas por limpar
                 if not rows_to_delete:
-                    tetrominos.append(Tetromino())
+                    blocks.append(Block())
+                    piece_drop_sound.play()
 
-                # Verificar si el juego ha terminado
-                if any(board[1]):  # Si hay algún bloque en la segunda fila del tablero
+                # Parar o xogo se unha peza chega ao límite superior
+                if any(board[1]): 
                     game_over = True
 
-                # Mostrar "GAME OVER" con un fondo verde si el tiempo se agota
+                # Parar o xogo se o tempo chega a 0
                 if countdown_timer == 0:
                     game_over = True
 
-        # Dibujar el tablero
+        # Debuxar tableiro
         for i in range(len(board)):
             for j in range(len(board[i])):
                 if board[i][j]:
                     pygame.draw.rect(screen, board[i][j], (j * BLOCK_SIZE + BOARD_OFFSET_X, i * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
 
-        # Dibujar el tetromino actual
-        current_tetromino.draw(screen)
+        # Debuxar peza
+        current_block.draw(screen)
 
-        # Mostrar "GAME OVER" si el juego ha terminado
+        # Cargar pantalla de GAME OVER ao rematar o xogo
         if game_over:
+            pygame.mixer.music.stop()  
+
+            if not game_over_sound_played:
+                game_over_sound_played = True
+                if countdown_timer == 0:
+                    win_sound.play()  
+                else: 
+                    lose_sound.play()
+                
+
             game_over_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
-            if countdown_timer == 0:
+            if countdown_timer == 0 and not ended:
                 game_over_surface.fill((0, 255, 0, 128))  # Fondo verde
             else:
-                game_over_surface.fill(RED_LIGHT + (128,))  # Fondo rojo
+                game_over_surface.fill(RED_LIGHT + (128,))  # Fondo vermello 
+                ended = True
             screen.blit(game_over_surface, (0, 0))
+
+            # Ventá emerxente coa mensaxe de GAME OVER e a Puntuación Final
+            game_over_surface = pygame.Surface((300, 100), pygame.SRCALPHA)
+            game_over_surface.fill(WHITE)  
+            game_over_rect = game_over_surface.get_rect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2))
+
             game_over_text = font.render("GAME OVER", True, BLACK)
-            game_over_rect = game_over_text.get_rect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - FONT_SIZE))
-            screen.blit(game_over_text, game_over_rect)
+            game_over_text_rect = game_over_text.get_rect(center=(150, 30))
+            game_over_surface.blit(game_over_text, game_over_text_rect)
+
+            final_score_text = font.render(f"Puntuación Final: {score}", True, BLACK)
+            final_score_text_rect = final_score_text.get_rect(center=(150, 70))
+            game_over_surface.blit(final_score_text, final_score_text_rect)
+
+            screen.blit(game_over_surface, game_over_rect)
+
+            countdown_timer = 0 # Paramos o tempo
 
         pygame.display.flip()
-        clock.tick(5)  # Velocidad de caída de los bloques inicial
+        clock.tick(30)  # Velocidade de fotogramas 
 
     pygame.quit()
 
