c64d451fdc69eafec2a4e51f14dbefe6510dfea7
Clara Cidrás Fernández <91012917+claracidras@users.noreply.github.com>
2024-02-29 17:46:05 +0100

    Added main, return and folder organized

--- Tetris/Tetris.py
+++ Tetris/Tetris.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 import time
-
+from block import Block
 
 pygame.init()
 
@@ -22,94 +22,18 @@ BLACK = (0, 0, 0)
 RED_LIGHT = (255, 128, 128)
 FONT_SIZE = 24
 
-# Formas das pezas
-blocks = [
-    [[[0, 2, 2],
-      [2, 2, 0]],  # Peza "S"
-     (0, 0, 255)],  # Azul
+# Cargar imagen de fondo  
+background_image = pygame.image.load('assets/background.jpeg')
 
-    [[[3, 3, 0],
-      [0, 3, 3]],  # Peza "Z"
-     (255, 165, 0)],  #  Naranxa
+# Música
+pygame.mixer.music.load('assets/music/tetris_song.ogg')
+pygame.mixer.music.play(-1)  
 
-    [[[4, 4, 4],
-      [0, 4, 0]],  # Peza "T"
-     (150, 75, 0)],  # Marrón
-
-    [[[0, 0, 5],
-      [5, 5, 5]],  # Peza "L"
-     (0, 255, 0)],  # Verde
-
-    [[[6, 6],
-      [6, 6]],  # Peza "O"
-     (255, 255, 0)],  # Amarelo
-
-    [[[7, 7, 7, 7]],  # Peza "I"
-     (255, 0, 0)]  # Vermalle
-]
-
-
-class Block:
-    def __init__(self):
-        self.x = random.randint(2, BOARD_WIDTH // BLOCK_SIZE - 4) * BLOCK_SIZE + BOARD_OFFSET_X 
-        self.y = 0
-        self.shape, self.color = random.choice(blocks)
-        self.falling = True
-    
-    def draw(self, surface):
-        for i in range(len(self.shape)):
-            for j in range(len(self.shape[i])):
-                if self.shape[i][j]:
-                    pygame.draw.rect(surface, self.color, (self.x + j * BLOCK_SIZE, self.y + i * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-                    pygame.draw.rect(surface, BLACK, (self.x + j * BLOCK_SIZE, self.y + i * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE), 1)
-
-    def move_down(self):
-        self.y += BLOCK_SIZE
-    
-    def move_left(self, board):
-        for i in range(len(self.shape)):
-            for j in range(len(self.shape[i])):
-                if self.shape[i][j]:
-                    if self.x + j * BLOCK_SIZE <= BOARD_OFFSET_X or (self.x + j * BLOCK_SIZE) // BLOCK_SIZE - 1 >= 0 and board[self.y // BLOCK_SIZE + i][(self.x + j * BLOCK_SIZE - BOARD_OFFSET_X) // BLOCK_SIZE - 1]:
-                        return
-        self.x -= BLOCK_SIZE
-
-    def move_right(self, board):
-        for i in range(len(self.shape)):
-            for j in range(len(self.shape[i])):
-                if self.shape[i][j]:
-                    if (self.x + j * BLOCK_SIZE >= BOARD_WIDTH + BOARD_OFFSET_X - BLOCK_SIZE or
-                        (self.x + j * BLOCK_SIZE - BOARD_OFFSET_X) // BLOCK_SIZE + 1 < len(board[0]) and board[self.y // BLOCK_SIZE + i][(self.x + j * BLOCK_SIZE - BOARD_OFFSET_X) // BLOCK_SIZE + 1]):
-                        return
-        self.x += BLOCK_SIZE
-    
-    def collide(self, board):
-        for i in range(len(self.shape)):
-            for j in range(len(self.shape[i])):
-                if self.shape[i][j]:
-                    if (self.y + i * BLOCK_SIZE >= BOARD_HEIGHT - BLOCK_SIZE or
-                        (self.y // BLOCK_SIZE + i + 1 < len(board) and self.x // BLOCK_SIZE + j - BOARD_OFFSET_X // BLOCK_SIZE < len(board[0]) and board[self.y // BLOCK_SIZE + i + 1][(self.x - BOARD_OFFSET_X + j * BLOCK_SIZE) // BLOCK_SIZE])):
-                        self.falling = False
-                        return True
-        return False
-
-    def can_rotate(self, board):
-        current_shape = [row[:] for row in self.shape]
-        rotated_shape = [[current_shape[j][i] for j in range(len(current_shape))] for i in range(len(current_shape[0]))]
-        rotated_shape.reverse()
-        for i in range(len(rotated_shape)):
-            for j in range(len(rotated_shape[i])):
-                if rotated_shape[i][j]:
-                    if (self.y + i * BLOCK_SIZE >= BOARD_HEIGHT or self.x + j * BLOCK_SIZE < BOARD_OFFSET_X or
-                        self.x + j * BLOCK_SIZE >= BOARD_WIDTH + BOARD_OFFSET_X or board[self.y // BLOCK_SIZE + i][(self.x - BOARD_OFFSET_X + j * BLOCK_SIZE) // BLOCK_SIZE]):
-                        return False
-        return True
-
-    def rotate(self, board):
-        if self.can_rotate(board):
-            current_shape = [row[:] for row in self.shape]
-            self.shape = [[current_shape[j][i] for j in range(len(current_shape))] for i in range(len(current_shape[0]))]
-            self.shape.reverse()
+# Efectos de sonido
+clear_row_sound = pygame.mixer.Sound('assets/music/line.wav')
+piece_drop_sound = pygame.mixer.Sound('assets/music/drop.mp3')
+win_sound = pygame.mixer.Sound('assets/music/win.mp3')
+lose_sound = pygame.mixer.Sound('assets/music/lose.mp3')
 
 
 
@@ -141,19 +65,6 @@ def main():
     # Contadores
     score = 0
     countdown_timer = 60
-
-    # Imaxe de fondo
-    background_image = pygame.image.load('background.jpeg')
-
-    # Música
-    pygame.mixer.music.load('tetris_song.ogg')
-    pygame.mixer.music.play(-1)  
-
-    # Efectos de sonido
-    clear_row_sound = pygame.mixer.Sound('line.wav')
-    piece_drop_sound = pygame.mixer.Sound('drop.mp3')
-    win_sound = pygame.mixer.Sound('win.mp3')
-    lose_sound = pygame.mixer.Sound('lose.mp3')
 
     running = True
     while running:
@@ -297,8 +208,10 @@ def main():
 
         pygame.display.flip()
         clock.tick(30)  # Velocidade de fotogramas 
+    
 
     pygame.quit()
+    return score
 
 if __name__ == "__main__":
     main()
---
+++ Tetris/block.py
@@ -0,0 +1,106 @@
+import pygame
+import random
+
+BLOCK_SIZE = 20
+BOARD_WIDTH = 200 
+BOARD_HEIGHT = 500 
+WINDOW_WIDTH = 800  
+BLACK = (0, 0, 0)
+
+BOARD_OFFSET_X = (WINDOW_WIDTH - BOARD_WIDTH) // 2 
+
+blocks = [
+    [[[0, 2, 2],
+      [2, 2, 0]],  # Peza "S"
+     (0, 0, 255)],  # Azul
+
+    [[[3, 3, 0],
+      [0, 3, 3]],  # Peza "Z"
+     (255, 165, 0)],  #  Naranxa
+
+    [[[4, 4, 4],
+      [0, 4, 0]],  # Peza "T"
+     (150, 75, 0)],  # Marrón
+
+    [[[0, 0, 5],
+      [5, 5, 5]],  # Peza "L"
+     (0, 255, 0)],  # Verde
+
+    [[[6, 6],
+      [6, 6]],  # Peza "O"
+     (255, 255, 0)],  # Amarelo
+
+    [[[7, 7, 7, 7]],  # Peza "I"
+     (255, 0, 0)]  # Vermalle
+]
+
+
+class Block:
+
+    def __init__(self):
+        self.x = random.randint(2, BOARD_WIDTH // BLOCK_SIZE - 4) * BLOCK_SIZE + BOARD_OFFSET_X 
+        self.y = 0
+        self.shape, self.color = random.choice(blocks)
+        self.falling = True
+    
+
+    def draw(self, surface):
+        for i in range(len(self.shape)):
+            for j in range(len(self.shape[i])):
+                if self.shape[i][j]:
+                    pygame.draw.rect(surface, self.color, (self.x + j * BLOCK_SIZE, self.y + i * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+                    pygame.draw.rect(surface, BLACK, (self.x + j * BLOCK_SIZE, self.y + i * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE), 1)
+
+
+    def move_down(self):
+        self.y += BLOCK_SIZE
+    
+
+    def move_left(self, board):
+        for i in range(len(self.shape)):
+            for j in range(len(self.shape[i])):
+                if self.shape[i][j]:
+                    if self.x + j * BLOCK_SIZE <= BOARD_OFFSET_X or (self.x + j * BLOCK_SIZE) // BLOCK_SIZE - 1 >= 0 and board[self.y // BLOCK_SIZE + i][(self.x + j * BLOCK_SIZE - BOARD_OFFSET_X) // BLOCK_SIZE - 1]:
+                        return
+        self.x -= BLOCK_SIZE
+
+
+    def move_right(self, board):
+        for i in range(len(self.shape)):
+            for j in range(len(self.shape[i])):
+                if self.shape[i][j]:
+                    if (self.x + j * BLOCK_SIZE >= BOARD_WIDTH + BOARD_OFFSET_X - BLOCK_SIZE or
+                        (self.x + j * BLOCK_SIZE - BOARD_OFFSET_X) // BLOCK_SIZE + 1 < len(board[0]) and board[self.y // BLOCK_SIZE + i][(self.x + j * BLOCK_SIZE - BOARD_OFFSET_X) // BLOCK_SIZE + 1]):
+                        return
+        self.x +=  BLOCK_SIZE
+    
+
+    def collide(self, board):
+        for i in range(len(self.shape)):
+            for j in range(len(self.shape[i])):
+                if self.shape[i][j]:
+                    if (self.y + i * BLOCK_SIZE >= BOARD_HEIGHT - BLOCK_SIZE or
+                        (self.y // BLOCK_SIZE + i + 1 < len(board) and self.x // BLOCK_SIZE + j - BOARD_OFFSET_X // BLOCK_SIZE < len(board[0]) and board[self.y // BLOCK_SIZE + i + 1][(self.x - BOARD_OFFSET_X + j * BLOCK_SIZE) // BLOCK_SIZE])):
+                        self.falling = False
+                        return True
+        return False
+
+
+    def can_rotate(self, board):
+        current_shape = [row[:] for row in self.shape]
+        rotated_shape = [[current_shape[j][i] for j in range(len(current_shape))] for i in range(len(current_shape[0]))]
+        rotated_shape.reverse()
+        for i in range(len(rotated_shape)):
+            for j in range(len(rotated_shape[i])):
+                if rotated_shape[i][j]:
+                    if (self.y + i * BLOCK_SIZE >= BOARD_HEIGHT or self.x + j * BLOCK_SIZE < BOARD_OFFSET_X or
+                        self.x + j * BLOCK_SIZE >= BOARD_WIDTH + BOARD_OFFSET_X or board[self.y // BLOCK_SIZE + i][(self.x - BOARD_OFFSET_X + j * BLOCK_SIZE) // BLOCK_SIZE]):
+                        return False
+        return True
+
+
+    def rotate(self, board):
+        if self.can_rotate(board):
+            current_shape = [row[:] for row in self.shape]
+            self.shape = [[current_shape[j][i] for j in range(len(current_shape))] for i in range(len(current_shape[0]))]
+            self.shape.reverse()
